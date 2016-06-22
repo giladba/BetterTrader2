dfb24bf59f51ecb48ddec3baf7857e6f6cef9bae
giladba <giladba@post.jce.ac.il>
2016-06-22 10:53:33 +0300

    Instrument tracking system works.

--- client/www/js/controllers/set-sense.js
+++ client/www/js/controllers/set-sense.js
@@ -1,7 +1,13 @@
-angular.module('app.set-sense', ['lbServices', 'ionic'])
-	.controller('SetSenseCtrl', function ($scope, Instrument) {
+angular.module('app')
+//angular.module('app.set-sense', ['lbServices', 'ionic'])
+	.controller('SetSenseCtrl', ['$rootScope', '$scope', 'Instrument', 'Client', 'User','InstrumentTracking','$location', function($rootScope, $scope, Instrument, Client, User, InstrumentTracking, $location) {
 		$scope.sense = 0.5;
-		$scope.submit = function() {
-			console.log("submit");
+		$scope.submit = function(instrument, newSense) {
+			console.log("submit " + JSON.stringify(instrument));
+			console.log("sense " + newSense);
+			console.log("instID " + instrument.id);
+			console.log("userID " + $rootScope.currentUser.id);
+
+			InstrumentTracking.create({sense:newSense, instID:instrument.id, userID:$rootScope.currentUser.id})
 		}
-	});
+	}]);
