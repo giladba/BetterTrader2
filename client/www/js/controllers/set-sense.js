fd49e7888c7b57b936d5f7f4475a3dd1f10387a3
giladba <giladba@post.jce.ac.il>
2016-06-23 13:29:23 +0300

    Changed the PK of instruments to be the name - this fixes a bug where the pk change and we can no longer see the tracked instrument.

--- client/www/js/controllers/set-sense.js
+++ client/www/js/controllers/set-sense.js
@@ -1,13 +1,12 @@
 angular.module('app')
-//angular.module('app.set-sense', ['lbServices', 'ionic'])
-	.controller('SetSenseCtrl', ['$rootScope', '$scope', 'Instrument', 'Client', 'User','InstrumentTracking','$location', function($rootScope, $scope, Instrument, Client, User, InstrumentTracking, $location) {
-		$scope.sense = 0.5;
-		$scope.submit = function(instrument, newSense) {
-			console.log("submit " + JSON.stringify(instrument));
-			console.log("sense " + newSense);
-			console.log("instID " + instrument.id);
-			console.log("userID " + $rootScope.currentUser.id);
+		.controller('SetSenseCtrl', ['$rootScope', '$scope', 'Instrument', 'Client', 'User','InstrumentTracking','$location', function($rootScope, $scope, Instrument, Client, User, InstrumentTracking, $location) {
+			$scope.sense = 0.5;
+			$scope.submit = function(instrument, newSense) {
+				console.log("submit " + JSON.stringify(instrument));
+				console.log("sense " + newSense);
+				console.log("instID " + instrument.name);
+				console.log("userID " + $rootScope.currentUser.id);
 
-			InstrumentTracking.create({sense:newSense, instID:instrument.id, userID:$rootScope.currentUser.id})
-		}
-	}]);
+				InstrumentTracking.create({sense:newSense, instID:instrument.name, userID:$rootScope.currentUser.id});
+			}
+		}]);
